632a87de32a04a3d3f4c999fc8f7530bc175ecb5
Crystal <Crystal@DESKTOP-81QTH9S>
2021-10-17 16:10:27 -0400

    Initial commit

---
+++ src/test/java/com/noorteck/qa/pages/POM.java
@@ -0,0 +1,9 @@
+package com.noorteck.qa.pages;
+
+public class POM {
+	
+	public static void main(String[] args) {
+		System.out.println("this is where will will put the elements as well as the related methods");
+	}
+
+}
