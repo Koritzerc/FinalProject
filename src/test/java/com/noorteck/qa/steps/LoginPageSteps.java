83f1a46de64492f11fe6a3ee326328a4ff770e2a
Crystal <Crystal@DESKTOP-81QTH9S>
2021-10-19 00:59:52 -0400

    Added login steps & runners

---
+++ src/test/java/com/noorteck/qa/steps/LoginPageSteps.java
@@ -0,0 +1,44 @@
+package com.noorteck.qa.steps;
+
+import com.noorteck.qa.pages.LoginPage;
+import com.noorteck.qa.utils.CommonUI;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginPageSteps {
+	
+	LoginPage loginPage;
+       
+	
+	@Given("User navigates to login page")
+	public void user_navigates_to_login_page() {
+	    String url = "http://demo.guru99.com/v4/index.php";
+	    CommonUI.openBrowser("chrome");
+	    CommonUI.navigate(url);
+	    
+	    loginPage = new LoginPage();
+	}
+
+	@When("User provides valid credential")
+	public void user_provides_valid_credential() {
+		loginPage.enterUserId("mngr360115");
+		loginPage.enterPassword("hYnajut");
+	}
+	   
+	
+
+	@Then("User clicks login button")
+	public void user_clicks_login_button() {
+		loginPage.click();
+	}
+	    
+
+	@Then("User verify {string} title is displayed")
+	public void user_verify_title_is_displayed(String string) {
+	 
+		System.out.println(CommonUI.getTitle());
+	}
+
+}
